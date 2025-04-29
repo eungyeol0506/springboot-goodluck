61d40f143f4acb3e3d8fc9570c200da0e64e265b
eungyeol0506 <janess1789@gmail.com>
2025-04-29 11:52:59 +0900

    refactor: user list 찾는 기능 미사용으로 주석처리 2025-04-29

--- src/main/java/com/example/goodluck/domain/UserRepository.java
+++ src/main/java/com/example/goodluck/domain/UserRepository.java
@@ -1,13 +1,12 @@
 package com.example.goodluck.domain;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
     public void save(MyUser user);    
     public Optional<MyUser> findByNo(Long no); 
     public Optional<MyUser> findById(String id);
-    public List<MyUser> selectAll();
+    // public List<MyUser> findAll();
     public void remove(Long no);
     public void update(MyUser user);
 }
