b38cd06122e045e8eb8fd43409691903e046016a
eungyeol0506 <janess1789@gmail.com>
2025-05-07 00:23:50 +0900

    feat: loginForm, logout 설정 disable 처리 2025-05-07

--- src/main/java/com/example/goodluck/config/SecurityConfig.java
+++ src/main/java/com/example/goodluck/config/SecurityConfig.java
@@ -2,15 +2,12 @@ package com.example.goodluck.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,27 +19,22 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/js/**", "/files/**", "/", "/login", "/regist", "/list", "/board/**").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/regist").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                                .anyRequest().authenticated()
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(auth -> 
+                auth
+                    .requestMatchers("/profile", "/profile/form").authenticated()
+                    .requestMatchers("/board/write", "/board/form").authenticated()
+                    .anyRequest().permitAll()
                 )
-                .formLogin(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable)
-        ;
+                .formLogin(form ->
+                    form.disable()
+                )
+                .logout(logout ->
+                    logout.disable())
+                ;
 
         return http.build();
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        return (web) -> web.ignoring().requestMatchers(
-            new AntPathRequestMatcher("/login", "POST"),
-            new AntPathRequestMatcher("/login", "GET")
-        );
     }
 }
